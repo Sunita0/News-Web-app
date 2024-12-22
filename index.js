1fa8e5ab5e019ef7dd7dcbad34958c6de34f91b6
Sunita0 <102029274+Sunita0@users.noreply.github.com>
2024-12-22 14:31:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+const NewsModel = require("./model/news.js")
+app.listen(3000,() => {});
+
+app.get("/",(req,res) =>{
+    res.json({name:"sunita"});
+});
+app.post("/api/addnews",async(req,res) =>{
+    try{
+        const news = await NewsModel.create(req.body);
+        res.status(200).json(news);
+        console.log(req.body);
+    } catch (error){
+        res.send(500);
+    }
+});
+app.get("/api/news",async(req,res) =>{
+    try{
+        const news = await NewsModel.find({});
+        res.status(200).json(news);
+        console.log(req.body);
+    } catch (error){
+        res.send(500);
+    }
+});
+app.get("/api/news/:id",async(req,res) =>{
+    try{
+        const { id } = req.params;
+        const news = await NewsModel.findById(id);
+        res.status(200).json(news);
+        console.log(req.body);
+    } catch (error){
+        res.send(500);
+    }
+});
+app.put("/api/news/:id",async(req,res) =>{
+    try{
+        const { id } = req.params;
+        const news = await NewsModel.findByIdAndUpdate(id,req.body);
+        if(!news){
+            return res.status(404).json({message : " News Not Found "});
+        }
+        const updatenews = await NewsModel.findById(id);
+        res.status(200).json(updatenews);
+        
+    } catch (error){
+        res.send(500);
+    }
+});
+app.delete("/api/news/:id",async(req,res) =>{
+    try{
+        const { id } = req.params;
+        const news = await NewsModel.findByIdAndDelete(id,req.body);
+        if(!news){
+            return res.status(404).json({message : " News Not Found "});
+        }
+        const updatenews = await NewsModel.findById(id);
+        res.status(200).json("Delete");
+        
+    } catch (error){
+        res.send(500);
+    }
+});
+mongoose
+  .connect("mongodb://localhost:27017/")
+  .then(() => {
+    console.log("connected to DB");
+  });
