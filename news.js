fe6d221b742dc71120df546dae6713d692474ac9
Sunita0 <102029274+Sunita0@users.noreply.github.com>
2024-12-28 01:48:44 +0530

    Add files via upload

--- news.js
+++ news.js
@@ -1,28 +1,43 @@
-const mongoose = require("mongoose");
-const NewSchema = mongoose.Schema(
-    {
-        headline {
-            type: String,
-            required: [true,"please enter headline"],
-        },
-        description {
-            type: String,
-            required: [true,"please enter description"],
-        },
-        link {
-            type: String,
-            required: false,
-            default:0,
-        },
-        img {
-            type: String,
-            required: false,
-            default:0,
-        },
-    },
-    {
-        Timestamp: true,
-    },
-);
-const NewsModel = mongoose.model("News",NewSchema);
-module.exports = NewsModel;
+import React from "react";
+import axios from "axios";
+import { useEffect,useState } from "react";
+const News = () => {
+    const[news,setnews] = useState([]);
+    useEffect(() =>{
+        axios
+          .get("http://localhost:3000/api/news")
+          .then((res) => {
+            console.log(res.data);
+            setnews(res.data);
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+          .finally(function(){
+
+          },[]);
+          return(
+            <div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          )
+    });
+};
+export default News;
